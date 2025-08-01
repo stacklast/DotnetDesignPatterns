30a4b06f418c51eb98fb4df9640cc1aa16074bb9
Edwin <softwareywebsoluciones@gmail.com>
2025-07-31 22:26:39 -0500

    Ejemplo para Dependency Injection Pattern

---
+++ DesignPatterns/DependencyInjectionPattern/Beer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.DependencyInjectionPattern
+{
+    public class Beer
+    {
+        private string _name;
+        private string _brand;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public Beer(string name, string brand)
+        {
+            _name = name;
+            _brand = brand;
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using DesignPatterns.DependencyInjectionPattern;
 using DesignPatterns.FactoryPattern;
 using DesignPatterns.Singleton;
 
@@ -13,7 +14,7 @@ log.Save("This is another log message.");
 var singleton = Log.Instance;
 var singleton2 = Log.Instance;
 Console.WriteLine($"Are both singleton instances the same? {singleton == singleton2}");
-Console.ReadLine();
+
 
 //FACTORY PATTERN EXAMPLE
 
@@ -25,3 +26,11 @@ storeSale.Sell(15);
 
 ISale internetSale = internetFactory.GetSale();
 internetSale.Sell(15);
+
+// Dependency Injection Example
+var beer = new Beer("Corona","Light");
+var drinkWithBeer = new DrinkWithBeer(2.0m, 0.5m, beer);
+drinkWithBeer.Build();
+
+
+Console.ReadLine();
