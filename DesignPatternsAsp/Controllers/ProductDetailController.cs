af4efd072d894cf20d83fe52a82b1ccdfb01bff1
Edwin <softwareywebsoluciones@gmail.com>
2025-07-31 22:14:35 -0500

    Implementacion Factory Method

---
+++ DesignPatternsAsp/Controllers/ProductDetailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternsAsp.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 20);
+
+            //products
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+
+            //total
+            ViewBag.TotalLocal = total + localEarn.Earn(total);
+            ViewBag.TotalForeign = total + foreignEarn.Earn(total);
+
+            return View();
+        }
+    }
+}
---
+++ Tools/Earn/ForeingEarn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class ForeingEarn: IEarn
+    {
+        private decimal _percentage;
+        private decimal _extra;
+        public ForeingEarn(decimal percentage, decimal extra)
+        {
+            _percentage = percentage;
+            _extra = extra;
+        }
+        public decimal Earn(decimal amount)
+        {
+            return (amount * _percentage) + _extra;
+        }
+    }
+}
